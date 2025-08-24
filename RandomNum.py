678bcb6e1f226ddd29ac8ae34eabe9704af16b04
Kunalgupta66 <126165374+Kunalgupta66@users.noreply.github.com>
2025-08-24 15:14:11 +0530

    First code

---
+++ RandomNum.py
@@ -0,0 +1,36 @@
+import random as rd
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():   
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+else:
+    print('Please type a number next time.')
+    quit()
+
+random_num = rd.randint(0, top_of_range)
+
+guess = 0
+
+while True:
+    guess += 1
+    user_guess = input("Make a next guess")
+    if user_guess.isdigit():   
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+ 
+    if user_guess == random_num:
+        print("you got it")
+        break
+    elif user_guess > random_num:
+        print("you were above the number")
+    else:
+        print("you were blow the number")
+
+print(f'You got it in {guess} guesses')
